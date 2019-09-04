3f263bb58706f70127cb04785d4aa9c741bbfbd0
tangjie133 <1334875503@qq.com>
2019-09-04 10:31:03 +0800

    2

--- IFTTT_MQTT_Weather.ts
+++ IFTTT_MQTT_Weather.ts
@@ -95,7 +95,7 @@ enum LOCATION {
  *Obloq implementation method.
  */
 //% weight=10 color=#008B00 icon="\uf1eb" block="ObloqIFTTTandWeather"
-namespace IFTTT-MQTT-Weather {
+namespace IFTTT_MQTT_Weather {
 
     //serial
     let OBLOQ_SERIAL_INIT = OBLOQ_BOOL_TYPE_IS_FALSE
